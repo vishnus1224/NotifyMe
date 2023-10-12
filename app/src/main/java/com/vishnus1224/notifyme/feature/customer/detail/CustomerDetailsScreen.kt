87f89220a74ada69c0e8c4f7a936927b6cf3a576
Vishnu Shirodker <vishnus1224@gmail.com>
2023-10-12 11:55:26 +0530

    Customer list screen is now a scaffold, add capitalize to enter name text field

--- app/src/main/java/com/vishnus1224/notifyme/feature/customer/detail/CustomerDetailsScreen.kt
+++ app/src/main/java/com/vishnus1224/notifyme/feature/customer/detail/CustomerDetailsScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,103 +23,109 @@ import com.vishnus1224.notifyme.arch.NavigationCommand
 
 @Composable
 fun CustomerDetailsScreen(
-    viewModel: CustomerDetailsViewModel,
-    navigator: (NavigationCommand) -> Unit,
-    modifier: Modifier = Modifier,
+  viewModel: CustomerDetailsViewModel,
+  navigator: (NavigationCommand) -> Unit,
+  modifier: Modifier = Modifier,
 ) {
 
-    val state = viewModel.state().collectAsStateWithLifecycle()
+  val state = viewModel.state().collectAsStateWithLifecycle()
 
-    LaunchedEffect(key1 = viewModel) {
-        viewModel.navigation().collect(navigator)
-    }
+  LaunchedEffect(key1 = viewModel) {
+    viewModel.navigation().collect(navigator)
+  }
 
-    CustomerDetailsScreen(
-        state = state.value,
-        onNameChanged = viewModel::onNameChanged,
-        onAddressChanged = viewModel::onAddressChanged,
-        onPhoneNumberChanged = viewModel::onPhoneNumberChanged,
-        onSaveClicked = viewModel::onSaveCustomerClicked,
-        modifier = modifier,
-    )
+  CustomerDetailsScreen(
+    state = state.value,
+    onNameChanged = viewModel::onNameChanged,
+    onAddressChanged = viewModel::onAddressChanged,
+    onPhoneNumberChanged = viewModel::onPhoneNumberChanged,
+    onSaveClicked = viewModel::onSaveCustomerClicked,
+    modifier = modifier,
+  )
 }
 
 @Composable
 private fun CustomerDetailsScreen(
-    state: CustomerDetailsState,
-    onNameChanged: (String) -> Unit,
-    onAddressChanged: (String) -> Unit,
-    onPhoneNumberChanged: (String) -> Unit,
-    onSaveClicked: (name: String, address: String, phoneNumber: String) -> Unit,
-    modifier: Modifier,
+  state: CustomerDetailsState,
+  onNameChanged: (String) -> Unit,
+  onAddressChanged: (String) -> Unit,
+  onPhoneNumberChanged: (String) -> Unit,
+  onSaveClicked: (name: String, address: String, phoneNumber: String) -> Unit,
+  modifier: Modifier,
 ) {
 
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+  Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.spacedBy(8.dp),
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+
+    CenterAlignedTopAppBar(
+      title = {
+        Text(text = stringResource(id = R.string.enter_customer_details))
+      }
+    )
+
+    TextField(
+      value = state.name,
+      onValueChange = { onNameChanged(it) },
+      modifier = Modifier.fillMaxWidth(),
+      label = {
+        Text(text = stringResource(id = R.string.enter_name))
+      },
+      keyboardOptions = KeyboardOptions.Default.copy(
+        capitalization = KeyboardCapitalization.Words,
+      )
+    )
+
+    TextField(
+      value = state.address,
+      onValueChange = { onAddressChanged(it) },
+      modifier = Modifier.fillMaxWidth(),
+      label = {
+        Text(text = stringResource(id = R.string.enter_address))
+      },
+      keyboardOptions = KeyboardOptions.Default.copy(
+        capitalization = KeyboardCapitalization.Sentences,
+      )
+    )
+
+    TextField(
+      value = state.phoneNumber,
+      onValueChange = { onPhoneNumberChanged(it) },
+      modifier = Modifier.fillMaxWidth(),
+      label = {
+        Text(text = stringResource(id = R.string.enter_phone_number))
+      },
+      keyboardOptions = KeyboardOptions.Default.copy(
+        keyboardType = KeyboardType.Phone,
+      ),
+    )
+
+    Button(
+      onClick = { onSaveClicked(state.name, state.address, state.phoneNumber) },
+      modifier = Modifier.fillMaxWidth(),
     ) {
-
-        CenterAlignedTopAppBar(
-            title = {
-                Text(text = stringResource(id = R.string.enter_customer_details))
-            }
-        )
-
-        TextField(
-            value = state.name,
-            onValueChange = { onNameChanged(it) },
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.enter_name))
-            },
-        )
-
-        TextField(
-            value = state.address,
-            onValueChange = { onAddressChanged(it) },
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.enter_address))
-            },
-        )
-
-        TextField(
-            value = state.phoneNumber,
-            onValueChange = { onPhoneNumberChanged(it) },
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = stringResource(id = R.string.enter_phone_number))
-            },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Phone,
-            ),
-        )
-
-        Button(
-            onClick = { onSaveClicked(state.name, state.address, state.phoneNumber) },
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            Text(text = "save")
-        }
+      Text(text = "save")
     }
+  }
 }
 
 @Preview
 @Composable
 private fun CustomerDetailsScreenPreview() {
-    CustomerDetailsScreen(
-        state = CustomerDetailsState(
-            name = "abc",
-            address = "122 Downling Street",
-            phoneNumber = "+44894928884",
-            inProgress = false,
-            errorMessage = "",
-        ),
-        onNameChanged = { },
-        onAddressChanged = {},
-        onPhoneNumberChanged = { },
-        onSaveClicked = { _, _, _ -> },
-        modifier = Modifier,
-    )
+  CustomerDetailsScreen(
+    state = CustomerDetailsState(
+      name = "abc",
+      address = "122 Downling Street",
+      phoneNumber = "+44894928884",
+      inProgress = false,
+      errorMessage = "",
+    ),
+    onNameChanged = { },
+    onAddressChanged = {},
+    onPhoneNumberChanged = { },
+    onSaveClicked = { _, _, _ -> },
+    modifier = Modifier,
+  )
 }
--- app/src/main/java/com/vishnus1224/notifyme/feature/customer/listing/ListCustomerScreen.kt
+++ app/src/main/java/com/vishnus1224/notifyme/feature/customer/listing/ListCustomerScreen.kt
@@ -6,10 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,21 +32,21 @@ fun ListCustomerScreen(
   navigator: (NavigationCommand) -> Unit,
   modifier: Modifier = Modifier,
 ) {
-    val state = viewModel.state().collectAsStateWithLifecycle()
+  val state = viewModel.state().collectAsStateWithLifecycle()
 
-    LaunchedEffect(key1 = viewModel) {
+  LaunchedEffect(key1 = viewModel) {
 
-        viewModel.getAllCustomers()
+    viewModel.getAllCustomers()
 
-        viewModel.navigation().collect(navigator)
-    }
+    viewModel.navigation().collect(navigator)
+  }
 
-    ListCustomerScreen(
-        state = state.value,
-        onCustomerClick = viewModel::onCustomerClick,
-        onAddClick = viewModel::onAddCustomerClick,
-        modifier = modifier,
-    )
+  ListCustomerScreen(
+    state = state.value,
+    onCustomerClick = viewModel::onCustomerClick,
+    onAddClick = viewModel::onAddCustomerClick,
+    modifier = modifier,
+  )
 }
 
 @Composable
@@ -51,59 +56,71 @@ private fun ListCustomerScreen(
   onAddClick: () -> Unit,
   modifier: Modifier,
 ) {
+  Scaffold(
+    topBar = {
+      CenterAlignedTopAppBar(
+        title = { Text(text = "List of customers") },
+      )
+    },
+    floatingActionButton = {
+      FloatingActionButton(onClick = onAddClick) {
+        Icon(Icons.Default.Add, contentDescription = "Add new customer")
+      }
+    },
+    modifier = modifier,
+  ) { paddingValues ->
 
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = modifier.padding(paddingValues),
+      verticalArrangement = Arrangement.spacedBy(8.dp),
+      horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        CenterAlignedTopAppBar(
-            title = { Text(text = "List of customers") },
-        )
 
-        Button(onClick = onAddClick) {
-            Text(text = "Add new")
-        }
+      if (state.customers.isNotEmpty()) {
+        LazyColumn {
+          itemsIndexed(state.customers) { index, customer ->
+            Text(
+              text = customer.name,
+              modifier = Modifier
+                  .fillMaxWidth()
+                  .clickable { onCustomerClick(customer) }
+                  .padding(16.dp)
+            )
 
-        if (state.customers.isNotEmpty()) {
-            LazyColumn {
-                items(state.customers) {
-                    Text(
-                        text = it.name,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onCustomerClick(it) }
-                            .padding(16.dp)
-                    )
-                }
+            // Add a divider if not the last item
+            if (index != state.customers.size - 1) {
+                Divider(modifier = Modifier.padding(horizontal = 16.dp))
             }
-        } else if (state.inProgress.not()) {
-            Text(text = "No customers found")
+          }
         }
+      } else if (state.inProgress.not()) {
+        Text(text = "No customers found")
+      }
 
-        if (state.errorMessage.isNotBlank()) {
-            Text(text = state.errorMessage)
-        }
+      if (state.errorMessage.isNotBlank()) {
+        Text(text = state.errorMessage)
+      }
 
-        if (state.inProgress) {
-            CircularProgressIndicator()
-        }
+      if (state.inProgress) {
+        CircularProgressIndicator()
+      }
     }
+  }
 }
 
 @Preview
 @Composable
 private fun ListCustomerScreenPreview() {
-    ListCustomerScreen(
-        state = ListCustomerState(
-            customers = listOf(
-                Customer("","abc", "122, Downing Street", "+4432839992", 0L)
-            ),
-            inProgress = false,
-            errorMessage = "",
-        ),
-        onCustomerClick = { },
-        onAddClick = { },
-        modifier = Modifier,
-    )
+  ListCustomerScreen(
+    state = ListCustomerState(
+      customers = listOf(
+        Customer("", "abc", "122, Downing Street", "+4432839992", 0L)
+      ),
+      inProgress = false,
+      errorMessage = "",
+    ),
+    onCustomerClick = { },
+    onAddClick = { },
+    modifier = Modifier,
+  )
 }
